570aaa4bff5d52eb135676368101e5a74abd2fb0
Habib Rehman <rehmank360@gmail.com>
2023-06-02 13:08:06 +0500

    Modiify app.vue for admin login

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,25 +1,22 @@
 <template>
 
-    <AdminLayout v-if="route.meta.isAdminRoute == true  &&  isLoggedIn">
-      <router-view />
-    </AdminLayout>
+  <AdminLayout v-if="route.meta.isAdminRoute == true && isLoggedIn">
+    <router-view />
+  </AdminLayout>
 
-    <SiteLayout v-else-if="route.meta.isUserRoute == true">
-      <router-view />
-    </SiteLayout>
+  <AdminLogin @response="checkLogin" v-else-if="route.meta.isAdminRoute == true && !isLoggedIn" />
 
-    <AdminLogin @response="checkLogin" v-else-if="route.name == 'AdminLogin'" />
+  <SiteLayout v-else-if="route.meta.isUserRoute == true">
+    <router-view />
+  </SiteLayout>
 
-    <Login @response="checkLogin" v-else-if="route.name == 'Login'"/>
-
-    <NotFound v-else />
+  <NotFound v-else />
 
 </template>
 
 <script setup>
 import AdminLayout from '@/layouts/Admin.vue'
 import NotFound from '@/404.vue'
-import Login from '@/site/pages/Login.vue'
 import SiteLayout from '@/layouts/Site.vue'
 import { useRoute } from "vue-router"
 import AdminLogin from "@/admin/pages/Login.vue"
@@ -30,7 +27,8 @@ const token = ref(localStorage.getItem('blogsAccessToken'))
 const isLoggedIn = ref(token.value !== null && token.value != '' ? true : false)
 
 const checkLogin = () => {
-  isLoggedIn.value = true
+
+isLoggedIn.value = true
 }
 
 </script>
